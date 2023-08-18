d40f6b52a5e8d80162d62f38f33b59bd2b5ba115
Victor Corbuleanu <46979394+vcorbuleanu@users.noreply.github.com>
2023-08-18 12:12:36 +0300

    Update validation schema to dynamic.

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useForm, Controller} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import {Form, Input, DatePicker, Button} from 'antd';
 
-const schema = yup.object().shape({
-    username: yup.string().required('Username is required'),
-    email: yup.string().email('Invalid email').required('Email is required'),
-    dob: yup.date().nullable().required('Date of Birth is required'),
-});
-
 const RegistrationForm = () => {
-    const {handleSubmit, control, formState, trigger} = useForm({
-        resolver: yupResolver(schema),
-    });
-
     const onSubmit = (data) => {
         console.log(data);
     };
+
+    const [showDob, setShowDob] = useState(false);
+
+    const schema = yup.object().shape({
+        username: yup.string().required('Username is required'),
+        email: yup.string().email('Invalid email').required('Email is required'),
+        ...(showDob
+            ? {
+                dob: yup.date().nullable().required('Date of Birth is required'),
+            }
+            : {}),
+    });
+
+    const {handleSubmit, control, formState, trigger} = useForm({
+        resolver: yupResolver(schema),
+    });
 
     return (
         <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
@@ -58,39 +64,41 @@ const RegistrationForm = () => {
                             onChange={(event) => {
                                 field.onChange(event);
                                 trigger('email').then(); // Trigger validation
+                                setShowDob(!formState.errors.email);
                             }}
                             onBlur={() => {
                                 trigger('email').then(); // Trigger validation on blur
+                                setShowDob(!formState.errors.email);
                             }}
                         />
                     )}
                 />
             </Form.Item>
-
-            <Form.Item
-                label="Date of Birth"
-                help={formState.errors.dob?.message}
-                validateStatus={formState.errors.dob?.message ? 'error' : ''}
-            >
-                <Controller
-                    name="dob"
-                    control={control}
-                    render={({field}) => (
-                        <DatePicker
-                            {...field}
-                            style={{width: '100%'}}
-                            onChange={(date) => {
-                                field.onChange(date);
-                                trigger('dob').then(); // Trigger validation
-                            }}
-                            onBlur={() => {
-                                trigger('dob').then(); // Trigger validation on blur
-                            }}
-                        />
-                    )}
-                />
-            </Form.Item>
-
+            {showDob && (
+                <Form.Item
+                    label="Date of Birth"
+                    help={formState.errors.dob?.message}
+                    validateStatus={formState.errors.dob?.message ? 'error' : ''}
+                >
+                    <Controller
+                        name="dob"
+                        control={control}
+                        render={({field}) => (
+                            <DatePicker
+                                {...field}
+                                style={{width: '100%'}}
+                                onChange={(date) => {
+                                    field.onChange(date);
+                                    trigger('dob').then(); // Trigger validation
+                                }}
+                                onBlur={() => {
+                                    trigger('dob').then(); // Trigger validation on blur
+                                }}
+                            />
+                        )}
+                    />
+                </Form.Item>
+            )}
             <Form.Item>
                 <Button type="primary" htmlType="submit" disabled={formState.isSubmitting}>
                     Submit
